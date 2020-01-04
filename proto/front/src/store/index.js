795f3a68eda0b0fff1be1d4200d81120c25740df
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-01-05 02:59:55 +0500

    data structure preparings

--- proto/front/src/store/index.js
+++ proto/front/src/store/index.js
@@ -5,6 +5,26 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    myID: 'myID',
+    profiles: {
+      myID: {
+        name: 'My tiny school',
+        flows: ['flow1', 'flow2'],
+        events: ['event1', 'event2'],
+        regularEvents: ['regEvent1', 'regEvent2'],
+        teachers: ['teacher1', 'teacher2'],
+        classes: ['class1', 'class2'],
+      },
+    },
+    flows: {
+      flow1: {
+        name: '',
+      },
+    },
+    events: {},
+    regularEvents: {},
+    teachers: {},
+    classes: {},
   },
   mutations: {
   },
