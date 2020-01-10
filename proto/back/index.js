c79606e1320a80222eca9e1124eb50adfb1f4b65
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-01-10 22:44:08 +0500

    mongodb, argon2 and eslint added into the project

--- proto/back/index.js
+++ proto/back/index.js
@@ -1,4 +1,21 @@
 const WebSocket = require('ws');
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017/";
+const mongoClient = new MongoClient(url, { useNewUrlParser: true });
+ 
+mongoClient.connect(function(err, client){
+  const db = client.db("sked-db");
+  const collection = db.collection("users");
+  let user = {name: "Tom", age: 23};
+  collection.insertOne(user, function(err, result){
+    if (err){ 
+      return console.log(err);
+    }
+    console.log(result.ops);
+    client.close();
+  });
+});
  
 const wss = new WebSocket.Server({ port: 8081 });
  
